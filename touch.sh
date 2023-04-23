7c943baf71ee38c136a2261e5e5f0b6245becde5
ayyappareddymopuri18 <115344859+ayyappareddymopuri18@users.noreply.github.com>
2023-04-23 22:09:06 +0530

    Delete touch.sh

--- touch.sh
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/pavanyendluri588/Yendluri Pavan Ram Chandar - Data Engineers.git
-git remote -v  --force
-git add . --force
-git commit -m "april_2nd_upload"
-
-
-git push -f origin master
-
